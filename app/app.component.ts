045c4ead1183c58e4b3b7429c1c5968f54f9f39e
Todd Motto <todd@toddmotto.com>
2016-12-31 10:16:47 +0000

    ⚡️ Release the seed

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div class="app">
+      Hello!
+    </div>
+  `
+})
+export class AppComponent {
+
+}
