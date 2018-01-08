ed9f2c1ea42853e0e41578f57612947c79fb2efc
Daniel Wulff <danielwulff.ee@gmail.com>
2018-01-08 14:56:31 -0800

    first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,13 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      {{ title }}
     </div>
   `
 })
 export class AppComponent {
-
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
