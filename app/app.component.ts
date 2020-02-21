353b634cbc2733616d0d8b91e064eaf4b5ba0ff7
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-02-21 14:39:00 -0600

    property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,25 +5,19 @@ import { Component }from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
   <div class='app'>
-    {{title + '!'}}
-    <div>
-      {{numberOne}}
-      {{numberTwo}}
-    </div>
-    <div>
-      {{numberOne + numberTwo}}
-    </div>
-    <div>
-      {{ isHappy ? ':)' : ':(' }}
-    </div>
-
+    <h1 [innerHTML]='title'></h1>
+    <h1>{{ title }}</h1>
+    <img [src]= 'logo'>
+    <input type='text' [value]='name'>
+    <div>{{ name }}</div>
   </div>
   `
-
 })
 
 export class AppComponent{
   title:string;
+  name: string = 'Todd';
+  logo: string = 'img/logo.svg'
   isHappy: boolean = true;
   numberOne: number =1;
   numberTwo: number =2;
