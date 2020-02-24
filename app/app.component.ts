2d1fd24a8dc5ffaabb537b3e3b146f633510057f
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-02-24 13:46:12 -0600

    Class ngclass

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,43 @@
 import { Component }from '@angular/core';
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
   <div class='app'>
     <h3>Airline Passengers</h3>
+    <ul>
+      <li *ngFor='let passenger of passengers; let i = index;'>
+      <span class='status'
+      [class.checked-in]='passenger.checkedIn'></span>
+      {{ i }}: {{ passenger.fullname }}
+      </li>
+    </ul>
+    <!--Multiple classnames -->
+    <h3>Airline Passengers</h3>
+    <ul>
+      <li *ngFor='let passenger of passengers; let i = index;'>
+      <span 
+        class='status'
+        [ngClass]="{
+          'checked-in': passenger.checkedIn,
+          'checked-out': !passenger.checkedIn        
+        }">
+      </span>
+        {{ i }}: {{ passenger.fullname }}
+      </li>
+    </ul>
   </div>
   `
 })
 
-export class AppComponent{
+export class AppComponent {
   passengers: Passenger[] =[{
     id: 1,
     fullname: 'Stephen',
@@ -24,7 +51,6 @@ export class AppComponent{
     fullname: 'Louise',
     checkedIn: true
   }, {
-  },{
     id: 5,
     fullname: 'Tina',
     checkedIn: false
