b2840c7f7c88609d05d8af14e928b4be6644c534
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-02-24 17:45:12 -0600

    Feature modules

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    // angular modules
     BrowserModule,
-    CommonModule
+    CommonModule,
+    // custom modules
+    PassengerDashboardModule
   ],
   bootstrap: [AppComponent]
 })
